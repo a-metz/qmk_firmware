30ff47a1b56bbee600ca155a870f5f35cc0f20de
Alexander Metz <alexander.wt.metz@gmail.com>
2023-02-14 21:01:19 +0100

    Enable pointing device and oled

--- keyboards/whtk/whtkwire_split/config.h
+++ keyboards/whtk/whtkwire_split/config.h
@@ -22,20 +22,26 @@
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN A9
 #define SERIAL_USART_RX_PIN A10
-// Swap TX and RX pins if keyboard is master halve. Disable this for boards with hardwired crossover.
-//#define SERIAL_USART_PIN_SWAP
+#define SERIAL_USART_PIN_SWAP
 #define SELECT_SOFT_SERIAL_SPEED 1
 #define SERIAL_USART_DRIVER SD1
 #define SERIAL_USART_TX_PAL_MODE 1
 #define SERIAL_USART_RX_PAL_MODE 1
 #define SERIAL_USART_TIMEOUT 20
 
-#define I2C_DRIVER        I2CD2
-#define I2C2_SCL_PIN      B6
-#define I2C2_SDA_PIN      B7
-#define I2C2_SCL_PAL_MODE 1
-#define I2C2_SDA_PAL_MODE 1
+#define I2C_DRIVER        I2CD1
+#define I2C1_SCL_PIN      B6
+#define I2C1_SDA_PIN      B7
+#define I2C1_SCL_PAL_MODE 1
+#define I2C1_SDA_PAL_MODE 1
 
 #define OLED_DISPLAY_128X64
 #define OLED_IC OLED_IC_SSD1306
 #define OLED_FONT_H "glcdfont_improved.c"
+
+#define POINTING_DEVICE_RIGHT
+#define SPLIT_POINTING_ENABLE
+
+#define CIRQUE_PINNACLE_POSITION_MODE CIRQUE_PINNACLE_ABSOLUTE_MODE
+#define CIRQUE_PINNACLE_TAP_ENABLE
+#define POINTING_DEVICE_GESTURES_CURSOR_GLIDE_ENABLE
--- keyboards/whtk/whtkwire_split/rules.mk
+++ keyboards/whtk/whtkwire_split/rules.mk
@@ -7,9 +7,11 @@ BOOTLOADER = stm32-dfu
 # Build Options
 KEYBOARD_SHARED_EP = yes
 EXTRAKEY_ENABLE = yes
-OLED_ENABLE = no
+OLED_ENABLE = yes
 SERIAL_DRIVER = usart
 DEBOUNCE_TYPE = sym_eager_pk
+POINTING_DEVICE_ENABLE = yes
+POINTING_DEVICE_DRIVER = cirque_pinnacle_i2c
 
 DEFAULT_FOLDER = whtk/whtkwire_split
 SRC +=  ../oled.c ../shift_alternative.c ../state.c
